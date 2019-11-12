b1218c86f8153ec3494b243cc7091ddffb910f43
sonu.sr <sonu.sr@speridian.com>
2019-11-12 14:19:08 -0500

    Spring Security

--- gateway-service/src/main/java/com/javalearn/gatewayservice/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/javalearn/gatewayservice/config/GatewayConfig.java
@@ -7,21 +7,30 @@ import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitio
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-/*@Configuration
+import org.springframework.core.env.Environment;
+
+import java.util.function.Function;
+
+@Configuration
 public class GatewayConfig {
 
-
+    @Autowired
+    Environment environment;
 
     @Bean
     public RouteLocator routeLocator (RouteLocatorBuilder routeLocatorBuilder) {
         System.out.println("service called");
         return routeLocatorBuilder.routes()
                 .route(r->r.path("/user/**")
+                        .filters(f->f.filter(new JWTFilter(environment)))
 //                        .uri("lb://USER-SERVICE/")
-                        .uri("http://localhost:8094/").filters(new JWTFilter().apply(new JWTFilter.Config()))
+                        .uri("http://localhost:8094/")
+
                         .id("employeeModule"))
                 .route(r->r.path("/customer/**")
                 .uri("lb://CUSTOMER-SERVICE/")
@@ -33,4 +42,3 @@ public class GatewayConfig {
                 .build();
     }
 }
-*/
--- gateway-service/src/main/java/com/javalearn/gatewayservice/config/JWTFilter.java
+++ gateway-service/src/main/java/com/javalearn/gatewayservice/config/JWTFilter.java
@@ -1,13 +1,16 @@
 
 package com.javalearn.gatewayservice.config;
 
-import io.jsonwebtoken.*;
-import org.apache.commons.lang.time.DateUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -20,22 +23,43 @@ import java.util.Date;
 import java.util.List;
 import java.util.function.Function;
 
-import static java.lang.System.*;
-
 
 @Component
-public class JWTFilter extends AbstractGatewayFilterFactory<JWTFilter.Config> {
+public class JWTFilter extends AbstractGatewayFilterFactory<JWTFilter.Config> implements GatewayFilter {
 
-    public JWTFilter() {
+    Environment environment;
+    public JWTFilter(Environment environment) {
         super(Config.class);
+        this.environment = environment;
     }
+
+    private static final Logger logger = LoggerFactory.getLogger(JWTFilter.class);
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("gateway filter");
+        ServerHttpRequest request = exchange.getRequest().mutate().header("Added-header", "12")
+                .build();
+        HttpHeaders values = exchange.getRequest().getHeaders();
+        if (!(exchange.getRequest().getPath().equals("login"))) {
+            List<String> jwtToken = values.getValuesAsList("Authorization");
+            System.out.println("gateway filter path if " + jwtToken);;
+            if (jwtToken.size() == 0) {
+                return this.onError(exchange, "No Authorization header", HttpStatus.UNAUTHORIZED);
+            }else {
+                System.out.println(getUsernameFromToken(jwtToken.get(0)));
+
+                return chain.filter(exchange.mutate().request(request).build());
+            }
+        }
+        return chain.filter(exchange.mutate().request(request).build());
+    }
+
     public static class Config {
         // Put the configuration properties
     }
 
-    @Value("${jwt.secret}")
-    private String secret;
-    private static final Logger logger = LoggerFactory.getLogger(JWTFilter.class);
 
     @Override
     public GatewayFilter apply(Config config) {
@@ -45,7 +69,7 @@ public class JWTFilter extends AbstractGatewayFilterFactory<JWTFilter.Config> {
             if (jwtToken.size() == 0) {
                 return this.onError(exchange, "No Authorization header", HttpStatus.UNAUTHORIZED);
             }else {
-                out.println(getUsernameFromToken(jwtToken.get(0)));
+                System.out.println(getUsernameFromToken(jwtToken.get(0)));
                 ServerHttpRequest request = exchange.getRequest().mutate().header("Added-header", "12")
                         .build();
                 return chain.filter(exchange.mutate().request(request).build());
@@ -60,7 +84,8 @@ public class JWTFilter extends AbstractGatewayFilterFactory<JWTFilter.Config> {
     private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         Date expiration = claims.getExpiration();
-        if (expiration.after(new Date())) {
+        System.out.println("Expiration "+expiration);
+        if (expiration.before(new Date())) {
             System.out.println("token expired");
             throw new ExpiredJwtException(null,claims,"Token Expired");
         }
@@ -70,7 +95,7 @@ public class JWTFilter extends AbstractGatewayFilterFactory<JWTFilter.Config> {
     private Claims getAllClaimsFromToken(String token) {
         String updatedToken = token.replace("Bearer ", "");
         try{
-            return Jwts.parser().setSigningKey(secret).parseClaimsJws(updatedToken).getBody();
+            return Jwts.parser().setSigningKey(environment.getProperty("secretKey")).parseClaimsJws(updatedToken).getBody();
         }catch (Exception e){
             throw new MalformedJwtException("Invalid Token");
         }
